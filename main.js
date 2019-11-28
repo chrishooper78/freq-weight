88448be17f139081d7eed2b44394f6eb7b384316
Chris Hooper <hoops78@protonmail.com>
2019-11-28 20:23:48 +0000

    loops to initalise array, calculate a-weighting

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-var data = [
-    ['0', '0', '0', '0', '0', '0', '0', '0'],
-    ['-26.2', '-16.1', '-8.6', '-3.2', '0', '1.2', '1', '-1.1'],
-    ['0', '0', '0', '0', '0', '0', '0', '0']
-];
+// initialise array (3x8)
+
+function Create2DArray(rows,columns) {
+    var x = new Array(rows);
+    for (var i = 0; i < rows; i++) {
+        x[i] = new Array(columns);
+    }
+    return x;
+ }
+
+var data = Create2DArray(3, 8);
+
+// set second row to "10"
+
+var freqBands = [62.5, 125, 250, 500, 1000, 2000, 4000, 8000]
+
+function RAf(f) {
+    var RAf = new Array(6);
+    RAf[0] = ((Math.pow(12194, 2)) * (Math.pow(f, 4)))
+    RAf[1] = ((Math.pow(f, 2)) + (Math.pow(20.6, 2)));
+    RAf[2] = ((Math.pow(f, 2)) + (Math.pow(107.7, 2)));
+    RAf[3] = ((Math.pow(f, 2)) + (Math.pow(737.9, 2)));
+    RAf[4] = ((Math.pow(f, 2)) + (Math.pow(12194, 2)));
+    RAf = RAf[0] / (RAf[1] * Math.sqrt(RAf[2] * RAf[3] * RAf[4]));    
+    return RAf;
+}
+
+function Af(freq) {
+    var Af = (20 * Math.log10(RAf(freq))) - (20 * Math.log10(RAf(1000)));
+    return Af;
+}
+
+var dataElementLength = data[1].length
+for (var i = 0; i < dataElementLength; i++) {
+    console.log(i);
+    data[1][i] = Af(freqBands[i]).toFixed(1);
+}
+
+// instantiate table
 
 var container = document.getElementById('example');
 
 var rowHeadersOctaves = ["Input", "", "Result"]
-var colHeadersOctaves = ["63", "125", "250", "500", "1000", "2000", "4000", "8000"]
+var colHeadersOctaves = freqBands
 
 var hot = new Handsontable(container, {
     data: data,
